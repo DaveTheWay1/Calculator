a8bcec99a26ac06ee442f944647432db19eb3ef5
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-12 12:58:12 -0700

    created a function to handle the eqaul sign. the expression is now there, very close to the final step

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ let total;
 let num1 = ''; 
 let operator = '';
 const operatorsArr = ['+', '-', '*', '/'];
+
 const output = document.querySelector('h1');
 const nums = document.querySelectorAll('.num');
+const equate = document.getElementById('solve');
+equate.addEventListener('click', solve);
 nums.forEach(num => num.addEventListener('click', handleNum1));
-
 const operators = document.querySelectorAll('.operator');
 operators.forEach(operator => operator.addEventListener('click', handleOperator));
 
 function handleNum1(e){
     const val = e.target.innerText;
-        if (!operatorsArr.includes(val) && val !== '='){
+        if (!operatorsArr.includes(val) && val){
             num1 += val; 
+            // total += num1;
+            // console.log(total);
+            console.log(num1)
             output.innerText = num1;
-        }else if(val === '='){
-            solve(e);
         }else{
             handleOperator(e);
         }
@@ -23,7 +26,8 @@ function handleNum1(e){
 
 function handleOperator(e) {
     const val = e.target.innerText;
-    console.log(val);
+    // console.log(val);
+    num1 += val;
     total = num1;
     console.log(total);
     num1 = '';
@@ -32,6 +36,8 @@ function handleOperator(e) {
 function solve(e){
     const val = e.target.innerText;
     console.log(val);
+    const problem = total + num1 + val;
+    console.log(problem);
 }
 
 
