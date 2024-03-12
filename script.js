ef3f5c5e60de14bcc51d6a1c956bec0228e95f11
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-12 09:45:48 -0700

    wip: the operators do not appear anymore, working on the solve problem when the user clicks =

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 let total;
 let num1 = ''; 
 let operator = '';
-// let num2 = ''
 const operatorsArr = ['+', '-', '*', '/'];
 const output = document.querySelector('h1');
 const nums = document.querySelectorAll('.num');
 nums.forEach(num => num.addEventListener('click', handleNum1));
 
 const operators = document.querySelectorAll('.operator');
-// operators.addEventListener('click', handleNum2);
+operators.forEach(operator => operator.addEventListener('click', handleOperator));
 
 function handleNum1(e){
-    const val = e.target;
-    // const containsOperator = operatorsArr.some(operator => num1.includes(operator));
-    // if (!containsOperator){
-        if (!operatorsArr.includes(val)){
-            num1 += val.innerText; 
+    const val = e.target.innerText;
+        if (!operatorsArr.includes(val) && val !== '='){
+            num1 += val; 
             output.innerText = num1;
-        } else if (operatorsArr.includes(val)){
-            console.log(val);
-            total = num1;
-            num1 = ''
+        }else if(val === '='){
+            solve(e);
+        }else{
+            handleOperator(e);
         }
-    // }
-//     else { 
-//         handleNum2();
-//     }
-// }
-
-    function handleNum2(e){
-        const val = e.target;
-
     }
+
+function handleOperator(e) {
+    const val = e.target.innerText;
+    console.log(val);
+    total = num1;
+    console.log(total);
+    num1 = '';
+}
+
+function solve(e){
+    const val = e.target.innerText;
+    console.log(val);
 }
 
 
